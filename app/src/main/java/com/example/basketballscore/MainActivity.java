c92756d02585e69569fb0561a360080afb14f58f
mina <mina077424@gmail.com>
2019-12-24 15:06:25 +0900

    Modify add bascketball score function

--- app/src/main/java/com/example/basketballscore/MainActivity.java
+++ app/src/main/java/com/example/basketballscore/MainActivity.java
@@ -8,32 +8,71 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    int total_score=0;
-    TextView scoreView;
+    int total_score_A=0;
+    int total_score_B=0;
+    TextView scoreView_A;
+    TextView scoreView_B;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        displayForTeamA(total_score);
+        scoreView_A = (TextView) findViewById(R.id.team_a_score);
+        scoreView_B = (TextView) findViewById(R.id.team_b_score);
+        displayForTeamA(total_score_A);
+        displayForTeamB(total_score_B);
     }
 
     /**
      * Displays the given score for Team A.
      */
     public void displayForTeamA(int score) {
-        scoreView = (TextView) findViewById(R.id.team_a_score);
-        scoreView.setText(String.valueOf(score));
+
+        scoreView_A.setText(String.valueOf(score));
     }
-    public void button3(View view){
-        total_score+=3;
-        scoreView.setText(String.valueOf(total_score));
+    public void button3A(View view){
+        final int shot3=3;
+        total_score_A+=shot3;
+        scoreView_A.setText(String.valueOf(total_score_A));
     }
-    public void button2(View view){
-        total_score+=2;
-        scoreView.setText(String.valueOf(total_score));
+    public void button2A(View view){
+        final int shot2=2;
+        total_score_A+=shot2;
+        scoreView_A.setText(String.valueOf(total_score_A));
     }
-    public void buttonFree(View view){
-        total_score+=1;
-        scoreView.setText(String.valueOf(total_score));
+    public void buttonFreeA(View view){
+        final int shotFree=1;
+        total_score_A+=shotFree;
+        scoreView_A.setText(String.valueOf(total_score_A));
+    }
+
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamB(int score) {
+        scoreView_B = (TextView) findViewById(R.id.team_b_score);
+        scoreView_B.setText(String.valueOf(score));
+    }
+    public void button3B(View view){
+        final int shot3=3;
+        total_score_B+=shot3;
+        scoreView_B.setText(String.valueOf(total_score_B));
+    }
+    public void button2B(View view){
+        final int shot2=2;
+        total_score_B+=shot2;
+        scoreView_B.setText(String.valueOf(total_score_B));
+    }
+    public void buttonFreeB(View view){
+        final int shotFree=1;
+        total_score_B+=shotFree;
+        scoreView_B.setText(String.valueOf(total_score_B));
+    }
+
+    public void reset(View view){
+        total_score_A=0;
+        total_score_B=0;
+        scoreView_A.setText(String.valueOf(total_score_A));
+        scoreView_B.setText(String.valueOf(total_score_B));
     }
 }
